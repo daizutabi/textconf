d4d6cfbe8652d46c902343c953445addd7080b69
Daizu <daizutabi@gmail.com>
2024-08-24 08:31:01 +0900

    Delete devcontainer

--- .devcontainer/postCreate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo 'eval "$(starship init bash)"' >> ~/.bashrc
-mkdir -p ~/.config
-cp .devcontainer/starship.toml ~/.config
-cargo install cargo-nextest --locked
--- src/types.rs
+++ src/types.rs
@@ -6,7 +6,7 @@ pub(crate) fn is_int(value: &str) -> bool {
 }
 
 pub(crate) fn is_float(value: &str) -> bool {
-    if value.is_empty() {
+    if value.len() <= 1 {
         return false;
     }
 
@@ -14,34 +14,26 @@ pub(crate) fn is_float(value: &str) -> bool {
     if re.is_match(value) {
         return true;
     }
-
     let value = value.to_lowercase();
+    let parts: Vec<&str> = value.split('e').collect();
+    if parts.len() != 2 {
+        return false;
+    }
 
-    //         return True
+    (is_float(parts[0]) || is_int(parts[0])) && is_int(parts[1])
 }
 
-// def is_float(x: str) -> bool:
-//     if len(x) <= 1:
-//         return False
+pub(crate) fn is_true(x: &str) -> bool {
+    x.to_lowercase() == "true"
+}
 
-//     if re.match(r"^[+-]?[0-9]*\.[0-9]*$", x):
-//         return True
+pub(crate) fn is_false(x: &str) -> bool {
+    x.to_lowercase() == "false"
+}
 
-//     x = x.lower()
-//     if x.count("e") == 1:
-//         a, b = x.split("e")
-//         return (is_float(a) or is_int(a)) and is_int(b)
-
-//     return False
-
-// def is_true(x: str) -> bool:
-//     return x.lower() == "true"
-
-// def is_false(x: str) -> bool:
-//     return x.lower() == "false"
-
-// def is_bool(x: str) -> bool:
-//     return is_true(x) or is_false(x)
+pub(crate) fn is_bool(x: &str) -> bool {
+    is_true(x) || is_false(x)
+}
 
 #[cfg(test)]
 mod tests {
@@ -61,55 +53,69 @@ mod tests {
         assert_eq!(is_int(format!("-{value}").as_str()), result);
         assert_eq!(is_int(format!("+-{value}").as_str()), false);
     }
+
+    #[rstest]
+    #[case("", false)]
+    #[case(".", false)]
+    #[case("0", false)]
+    #[case("1.0", true)]
+    #[case("1.", true)]
+    #[case(".1", true)]
+    #[case("a", false)]
+    #[case("ab", false)]
+    #[case("aeb", false)]
+    #[case("1e1", true)]
+    #[case("1e+1", true)]
+    #[case("1e-1", true)]
+    #[case("1e++1", false)]
+    #[case("1e--1", false)]
+    #[case("1e+-1", false)]
+    #[case("1e1.1", false)]
+    #[case("1.1e1", true)]
+    #[case("1.1e1", true)]
+    fn test_is_float(#[case] value: &str, #[case] result: bool) {
+        assert_eq!(is_float(value), result);
+        if result {
+            assert_eq!(is_float(format!("+{value}").as_str()), true);
+            assert_eq!(is_float(format!("-{value}").as_str()), true);
+            assert_eq!(is_float(value.to_uppercase().as_str()), true);
+        }
+    }
+
+    #[rstest]
+    #[case("true", true)]
+    #[case("TRUE", true)]
+    #[case("True", true)]
+    #[case("false", false)]
+    #[case("FALSE", false)]
+    #[case("False", false)]
+    #[case("other", false)]
+    fn test_is_true(#[case] input: &str, #[case] expected: bool) {
+        assert_eq!(is_true(input), expected);
+    }
+
+    #[rstest]
+    #[case("false", true)]
+    #[case("FALSE", true)]
+    #[case("False", true)]
+    #[case("true", false)]
+    #[case("TRUE", false)]
+    #[case("True", false)]
+    #[case("other", false)]
+    fn test_is_false(#[case] input: &str, #[case] expected: bool) {
+        assert_eq!(is_false(input), expected);
+    }
+
+    #[rstest]
+    #[case("true", true)]
+    #[case("TRUE", true)]
+    #[case("True", true)]
+    #[case("false", true)]
+    #[case("FALSE", true)]
+    #[case("False", true)]
+    #[case("other", false)]
+    #[case("", false)]
+    fn test_is_bool(#[case] input: &str, #[case] expected: bool) {
+        assert_eq!(is_bool(input), expected);
+    }
 }
-// import pytest
-
-// @pytest.mark.parametrize(
-//     ["x", "a"], [("", False), ("0", True), ("9", True), ("1.0", False), ("a", False)]
-// )
-// def test_is_int(x, a):
-//     from textconf.types import is_int
-
-//     assert is_int(x) == a
-//     assert is_int(f"+{x}") == a
-//     assert is_int(f"-{x}") == a
-//     assert is_int(f"++{x}") is False
-
-// @pytest.mark.parametrize(
-//     ["x", "a"],
-//     [
-//         ("", False),
-//         (".", False),
-//         ("0", False),
-//         ("1.0", True),
-//         ("1.", True),
-//         (".1", True),
-//         ("a", False),
-//         ("ab", False),
-//         ("aeb", False),
-//         ("1e1", True),
-//         ("1e+1", True),
-//         ("1e-1", True),
-//         ("1e++1", False),
-//         ("1e--1", False),
-//         ("1e+-1", False),
-//         ("1e1.1", False),
-//         ("1.1e1", True),
-//         ("1.1e1", True),
-//     ],
-// )
-// def test_is_float(x, a):
-//     from textconf.types import is_float
-
-//     assert is_float(x) == a
-//     if a:
-//         assert is_float(f"+{x}") is True
-//         assert is_float(f"-{x}") is True
-
-// @pytest.mark.parametrize(
-//     ["x", "a"], [("true", True), ("True", True), ("false", True), ("False", True)]
-// )
-// def test_is_bool(x, a):
-//     from textconf.types import is_bool
-
-//     assert is_bool(x) == a
