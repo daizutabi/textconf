1d160e913a2998ac4bd18fda425171b724bf39ec
Daizu <daizutabi@gmail.com>
2024-09-28 13:34:30 +0900

    python

--- tests/common/mod.rs
+++
@@ -1,9 +0,0 @@
-use anyhow::Result;
-
-pub fn read_example(path: &str) -> Result<Vec<String>> {
-    let text = std::fs::read_to_string(path)?;
-
-    let parts: Vec<&str> = text.split("---").collect();
-    assert!(parts.len() == 3);
-    Ok(parts.into_iter().map(|x| x.trim().to_string()).collect())
-}
